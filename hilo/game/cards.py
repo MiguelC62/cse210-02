843fe81f34190b284f79f78d084d81002489cd2a
Miguel Condori <miguel.angel.condori@gmail.com>
2022-01-27 19:55:43 -0300

    version with 3 times to wrong

--- hilo/game/cards.py
+++ hilo/game/cards.py
@@ -3,7 +3,7 @@ import random
 class Card:
     """A set of 13 different cards and the director shows one at a time. 
     The player must guess if the new card is higher or lower than the 
-    previous one.
+    previous one. The player has three tries to be wrong.
    
     Attributes:
         value (int): The number of points.
@@ -24,7 +24,7 @@ class Card:
             self (Card): An instance of Card.
         """
         self.value = random.randint(1,13)
-        
+        """
         #Sets the values of each number 1-11 to reflect a card suite,
         #for example, A for Ace(1), J for Jack(11), Q for Queen(12)
         #K for King(13)
@@ -38,3 +38,4 @@ class Card:
             self.value = "K"
         else:
             self.value = self.value
+        """
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -9,6 +9,7 @@ class Director:
         is_playing (boolean): Whether or not the game is being played.
         score (int): The score for one round of play.
         guess (boolean): If the gamer guesses correctly
+        lives (int): The lives's game for wrong guess
     """
 
     def __init__(self):
@@ -20,6 +21,7 @@ class Director:
         self.is_playing = True
         self.score = 300
         self.guess = True
+        self.lives = 3
 
     def start_game(self):
         """Starts the game by running the main game loop.
@@ -27,7 +29,7 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
-        while self.is_playing:
+        while self.is_playing and self.lives > 0:
             self.get_inputs()
             self.do_updates()
             self.do_outputs()
@@ -38,38 +40,38 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
-        lives = 3
-        while lives > 0:
-            card1 = Card()
-            card1.show()
-            print()
-            print(f"The card is: {card1.value}")
-            choice = input("Higer or lower? [h/l] ")
-            card2 = Card()
-            card2.show()
-            print(f"Next card was: {card2.value}")
+        #lives = 3
+    #while self.lives > 0:
+        card1 = Card()
+        card1.show()
+        print()
+        print(f"The card is: {card1.value}")
+        choice = input("Higer or lower? [h/l] ")
+        card2 = Card()
+        card2.show()
+        print(f"Next card was: {card2.value}")
+    
+        #Error handling for TypeError (int and str)
         
-            #Error handling for TypeError (int and str)
-            
-            try:
-                if (card1.value > card2.value) and choice == "l":
-                    self.guess = True
-                    break
-                elif (card1.value < card2.value) and choice == "h":
-                    self.guess = True
-                    break
-                else:
-                    self.guess = False
-                    lives = lives -1
-                    print(f"You are left with {lives} tries.")
-                    if lives == 0:
-                        self.is_playing = False
-                        print("Game Over")
-                    else:
-                        self.guess = False
-                        break
-            except TypeError as err:
-                pass
+        try:
+            if (card1.value > card2.value) and choice == "l":
+                self.guess = True
+                #break
+            elif (card1.value < card2.value) and choice == "h":
+                self.guess = True
+                #break
+            else:
+                self.guess = False
+                self.lives -= 1
+                print(f"You are left with {self.lives} tries.")
+                if self.lives == 0:
+                    #self.is_playing = False
+                    print("Game Over")
+                #else:
+                    # self.guess = False
+                    #break
+        except TypeError as err:
+            pass
        
     def do_updates(self):
         """Updates the player's score.
@@ -91,13 +93,18 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
+
         if not self.is_playing:
             return
 
-        print(f"Your score is: {self.score}")
-
-        if self.score > 0:
+        #if self.score > 0 and self.lives == 0:
+        if self.lives == 0:
+            print(f"Your final score was: {self.score}")
             answer = input("Play again? [y/n] ")
             self.is_playing = (answer == "y")
+            self.lives = 3
+        elif self.score > 0:
+            print(f"Your score is: {self.score}")
+            self.is_playing = True
         else:
             self.is_playing = False
