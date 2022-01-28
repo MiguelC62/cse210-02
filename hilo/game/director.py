d339aaee9f9b7a9e5eb2e51afa8d82bd4db2c4c3
Greenz081081 <87385409+Greenz081081@users.noreply.github.com>
2022-01-28 06:26:17 +0100

    Add files via upload
    Director upgrade

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,15 +1,14 @@
+from dbm.dumb import error
 from game.cards import Card
 
 class Director:
     """A person who directs the game. 
     
     The responsibility of a Director is to control the sequence of play.
-
     Attributes:
         is_playing (boolean): Whether or not the game is being played.
         score (int): The score for one round of play.
         guess (boolean): If the gamer guesses correctly
-        lives (int): The lives's game for wrong guess
     """
 
     def __init__(self):
@@ -29,6 +28,7 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
+        print()
         print("You earn 100 points when you hit and lose 75 when you miss.")
         print(f"Your initial score is: {self.score}")
         while self.is_playing and self.lives > 0:
@@ -38,46 +38,68 @@ class Director:
 
     def get_inputs(self):
         """Ask the user if card is high or low.
-
         Args:
             self (Director): An instance of Director.
         """
-        card1 = Card()
-        card1.show()
-        card1.suite_card()
-        print()
-        print("The card is: " + card1.string_value)
-        choice = input("Higer or lower? [h/l] ")
-        card2 = Card()
-        card2.show()
-        card2.suite_card()
-        print("Next card was: " + card2.string_value)
-    
-        #Error handling for TypeError (int and str)
-        
         try:
+            card1 = Card()
+            card1.show()
+            card1.suite_card()
+            print()
+            print("The card is:" + card1.string_value)
+            
+            choice = str(input("Higher or lower? [h/l]: "))
+
+            if choice.isalpha(): 
+                card2 = Card()
+                card2.show()
+                card2.suite_card()
+                print("Next card was:" + card2.string_value)
+
+            else: 
+                self.guess = False
+                self.lives -=1
+                print("Invalid... Only strings allowed!")
+                print(f"You are left with {self.lives} tries")
+
+                if self.lives == 0:
+                    self.is_playing = False
+                    print("Game Over")
+                    answer = input("Play again? [y/n] ")
+                    self.is_playing = (answer == "y")
+                return
+  
+
             if (card1.value > card2.value) and choice == "l":
                 self.guess = True
-                #break
+                # break
+
             elif (card1.value < card2.value) and choice == "h":
                 self.guess = True
-                #break
+                # break  
+
             else:
                 self.guess = False
-                self.lives -= 1
+                self.lives -=1
                 print(f"You are left with {self.lives} tries.")
+
                 if self.lives == 0:
-                    #self.is_playing = False
+                    self.is_playing = False
                     print("Game Over")
-                #else:
-                    # self.guess = False
-                    #break
+                    answer = input("Play again? [y/n] ")
+                    self.is_playing = (answer == "y")
+
         except TypeError as err:
-            pass
+            print(err)
+
+      
+
+
+       
+            
        
     def do_updates(self):
         """Updates the player's score.
-
         Args:
             self (Director): An instance of Director.
         """
@@ -91,7 +113,6 @@ class Director:
 
     def do_outputs(self):
         """Displays the cards and the score. Also asks the player if they want to play again. 
-
         Args:
             self (Director): An instance of Director.
         """
@@ -110,3 +131,5 @@ class Director:
             self.is_playing = True
         else:
             self.is_playing = False
+
+       
